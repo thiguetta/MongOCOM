2d1113fdc3766da484dc3e640c57dd0b5a7ffff7
javaboybr <thi.winnt@gmail.com>
2014-04-21 22:50:05 -0300

    retrieving config file setup method

--- src/main/java/com/arquivolivre/mongocom/management/CollectionManagerFactory.java
+++ src/main/java/com/arquivolivre/mongocom/management/CollectionManagerFactory.java
@@ -18,9 +18,18 @@ package com.arquivolivre.mongocom.management;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
+import com.mongodb.MongoURI;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.UnknownHostException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 
 /**
  *
@@ -30,8 +39,8 @@ public final class CollectionManagerFactory {
 
     private static Mongo client;
     private static final Logger LOG = Logger.getLogger(CollectionManagerFactory.class.getName());
-    //private static final String[] FILES = {"application", "database"};
-    //private static final String[] EXTENTIONS = {".conf", ".config", ".properties"};
+    private static final String[] FILES = {"application", "database"};
+    private static final String[] EXTENTIONS = {".conf", ".config", ".properties"};
 
     public static CollectionManager createCollectionManager() {
         return createBaseCollectionManager("", 0, "", "", "");
@@ -75,10 +84,9 @@ public final class CollectionManagerFactory {
         return null;
     }
 
-    /*TODO: improve the way config file is read
-    public static CollectionManager setup() {
+    public static CollectionManager setup(ServletContext context) {
         try {
-            File props = getPropertiesFile();
+            File props = getPropertiesFile(context);
             if (props == null) {
                 throw new FileNotFoundException("application or database configuration file not found.");
             }
@@ -119,31 +127,14 @@ public final class CollectionManagerFactory {
         return null;
     }
 
-    private static File getPropertiesFile() throws FileNotFoundException {
-        URI uri = null;
-        String s = System.getProperty("user.dir");
-        LOG.log(Level.INFO, s);
-        try {
-            URL url = CollectionManagerFactory.class.getProtectionDomain().getCodeSource().getLocation();
-            LOG.log(Level.INFO, url.toString());
-            boolean isVsf = url.toString().startsWith("vfs:");
-            if (isVsf) {
-                //needed for jboss
-                URLConnection conn = url.openConnection();
-                VirtualFile vf = (VirtualFile) conn.getContent();
-                uri = vf.getPhysicalFile().toURI();
-                LOG.log(Level.INFO, uri.toString());
-            } else {
-                uri = url.toURI();
-            }
-        } catch (IOException | URISyntaxException ex) {
-            LOG.log(Level.SEVERE, null, ex);
+    private static File getPropertiesFile(ServletContext context) throws FileNotFoundException {
+        String contextPath;
+        if (context != null) {
+            contextPath = context.getRealPath("WEB-INF");
+        } else {
+            contextPath = System.getProperty("user.dir");
         }
-        File parent = new File(uri);
-        parent = parent.getParentFile().getParentFile();
-        LOG.log(Level.INFO, parent.listFiles().toString());
-        LOG.log(Level.INFO, parent.getAbsolutePath());
-        File dir = new File(parent.getAbsolutePath() + "/conf");
+        File dir = new File(contextPath + "/conf");
         LOG.log(Level.INFO, dir.getAbsolutePath());
         File result = null;
         if (!dir.isDirectory()) {
@@ -173,5 +164,6 @@ public final class CollectionManagerFactory {
             }
         }
         return result;
-    }*/
+    }
+
 }
